6d0ebb44bec08dbc0e31a00865c03acd90418d31
wildorJulcaCachay <julcacachaywildor@gmail.com>
2025-12-04 12:14:12 -0500

    configuracion api keys

--- lib/intance.ts
+++ lib/intance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://10.152.21.245:3110/api",
+  baseURL: "https://server-chifa.onrender.com/api",
   withCredentials: true,
 });
 
-
+// const instance = "https://server-chifa.onrender.com/api"
 export default instance
